a1e0ac6a17040f1ef42be114328103aa5a0ca62b
ZOU0410 <flutter0u@gmail.com>
2023-11-24 21:02:57 +0800

    update

--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,15 +1,15 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_545c1d3f.mjs';
+import { manifest } from './manifest_0d5edc15.mjs';
 
 const _page0  = () => import('./chunks/generic_517c4a70.mjs');
 const _page1  = () => import('./chunks/index_c98a5c0a.mjs');
 const _page2  = () => import('./chunks/folio-1_d8631712.mjs');
 const _page3  = () => import('./chunks/contact_dd47fe57.mjs');
-const _page4  = () => import('./chunks/post-1_9fdfb892.mjs');
-const _page5  = () => import('./chunks/post-2_c3e8c7c0.mjs');
-const _page6  = () => import('./chunks/post-3_ffe8568c.mjs');
-const _page7  = () => import('./chunks/about_25e887f8.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/portfolio/folio-1.html", _page2],["src/pages/contact.html", _page3],["src/pages/post-1.md", _page4],["src/pages/post-2.html", _page5],["src/pages/post-3.md", _page6],["src/pages/about.astro", _page7]]);
+const _page4  = () => import('./chunks/about_25e887f8.mjs');
+const _page5  = () => import('./chunks/post-1_09206ba1.mjs');
+const _page6  = () => import('./chunks/post-2_c3e8c7c0.mjs');
+const _page7  = () => import('./chunks/post-3_92a50efd.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/portfolio/folio-1.html", _page2],["src/pages/contact.html", _page3],["src/pages/about.astro", _page4],["src/pages/blog/post-1.md", _page5],["src/pages/blog/post-2.html", _page6],["src/pages/blog/post-3.md", _page7]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
